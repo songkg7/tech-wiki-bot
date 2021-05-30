a0e53c2479b7f18377ad584cfc73113f9f08d9de
Haril Song <songkg7@gmail.com>
2021-05-30 11:01:26 +0900

    refactor: java 설명 호출 간결하게

--- main.py
+++ main.py
@@ -40,9 +40,7 @@ def handler(update, context):
     if user_text in greeting:
         bot.sendMessage(chat_id=chat_id, text="안녕하세요!")
 
-    elif "java" in user_text:
-        bot.sendMessage(chat_id=chat_id, text=java_info)
-    elif "자바" in user_text:
+    elif "java" or "자바" in user_text:
         bot.sendMessage(chat_id=chat_id, text=java_info)
 
     elif "python" or "파이썬" in user_text:
